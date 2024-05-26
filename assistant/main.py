2a40e696119e84aea6fe84e3341635555df0254d
Elie Khoury <elie.kh@gmail.com>
2024-05-26 12:19:27 +0200

    gmail integration

--- assistant/main.py
+++ assistant/main.py
@@ -2,6 +2,7 @@ from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings, 
 from llama_index.core.chat_engine.types import ChatMode
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
+from llama_index.readers.google import GmailReader
 
 import logging
 import sys
@@ -16,18 +17,24 @@ Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
 # ollama
 Settings.llm = Ollama(model="llama3", request_timeout=360.0)
 
+gmail_reader = GmailReader(service=None, results_per_page=10)
+
 # check if storage already exists
 PERSIST_DIR = "./.storage"
 if not os.path.exists(PERSIST_DIR):
     # load the documents and create the index
-    documents = SimpleDirectoryReader(".data").load_data()
-    index = VectorStoreIndex.from_documents(documents)
+    #documents = SimpleDirectoryReader(".data").load_data()
+    emails = gmail_reader.load_data()
+
+    index = VectorStoreIndex.from_documents(emails) #.from_documents(documents)
     # store it for later
     index.storage_context.persist(persist_dir=PERSIST_DIR)
 else:
     # load the existing index
     storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
     index = load_index_from_storage(storage_context)
+
+
 
 chat_engine = index.as_chat_engine(chat_mode=ChatMode.CONDENSE_PLUS_CONTEXT)
 chat_engine.reset()
