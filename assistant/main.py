0e12def37913adb3448f5cd079afeddb1e74de32
Elie Khoury <elie.kh@gmail.com>
2024-12-28 08:25:13 +0100

    more tools

--- assistant/main.py
+++ assistant/main.py
@@ -17,8 +17,8 @@ from llama_index.core import PromptTemplate
 import json;
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 from llama_index.embeddings.ollama import OllamaEmbedding
-from tools.code_interpreter import CodeInterpreterToolSpec
-from tools.date_time_retriever import CurrentDateTimeToolSpec
+from llama_index.tools.code_interpreter import CodeInterpreterToolSpec
+from tools.date_time_retriever import CurrentDateTimeToolSpec, DatetimeToolFnSchema
 from llama_index.tools.vector_db.base import VectorDBToolSpec
 from llama_index.core.agent import AgentRunner
 from llama_index.agent.coa import CoAAgentWorker
@@ -35,6 +35,7 @@ from llama_index.llms.azure_openai import AzureOpenAI
 from llama_index.embeddings.azure_openai import AzureOpenAIEmbedding
 from llama_index.core.callbacks import CallbackManager
 from llama_index.callbacks.aim import AimCallback
+from llama_index.tools.yahoo_finance import YahooFinanceToolSpec
 
 logging.basicConfig(stream=sys.stdout, level=logging.ERROR)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
@@ -46,7 +47,7 @@ nest_asyncio.apply()
 # azure_endpoint = "https://xxx.openai.azure.com/"
 # api_version = "2024-05-01-preview"
 
-# llm = AzureOpenAI(
+# azure_llm = AzureOpenAI(
 #     model="gpt-4o-mini",
 #     deployment_name="gpt-4o-mini",
 #     api_key=api_key,
@@ -182,13 +183,24 @@ def chat():
     todays_info_agent = ReActAgent.from_tools(
         tools=todays_info_spec.to_tool_list(), llm=Settings.llm, verbose=True, callback_manager=callback_manager)
 
+    finances_spec = YahooFinanceToolSpec()
+    finances_agent = ReActAgent.from_tools(
+        tools=finances_spec.to_tool_list(), llm=Settings.llm, verbose=True, callback_manager=callback_manager)
+    
     code_interpreter_spec = CodeInterpreterToolSpec()
     code_interpreter_agent = ReActAgent.from_tools(
         tools=code_interpreter_spec.to_tool_list(), llm=Settings.llm, verbose=True, callback_manager=callback_manager)
     
     date_engine = QueryEngineTool(query_engine=todays_info_agent ,metadata=ToolMetadata(
             name="current_date_and_time",
-            description="A useful set of functions that return the current date and time. They take as input the format and the timezone."
+            description="A useful set of functions that return the current date and time. They take as input the format and the timezone.",
+            fn_schema=DatetimeToolFnSchema
+        )
+    )
+
+    finance_engine = QueryEngineTool(query_engine=finances_agent ,metadata=ToolMetadata(
+            name="yahoo_finances",
+            description="A useful tool to exctract realtime financial information."
         )
     )
 
@@ -206,7 +218,7 @@ def chat():
         )
     )
 
-    tools = [date_engine, code_interpreter_engine, email_reader_engine]
+    tools = [date_engine, code_interpreter_engine, email_reader_engine, finance_engine]
     agent = ReActAgent.from_tools(
         tools=tools, llm=Settings.llm, verbose=True, memory=composable_memory, callback_manager=callback_manager, max_iterations=25)
 
--- assistant/tools/date_time_retriever.py
+++ assistant/tools/date_time_retriever.py
@@ -3,24 +3,24 @@
 import datetime
 from llama_index.core.tools.tool_spec.base import BaseToolSpec
 from zoneinfo import ZoneInfo
+from pydantic import BaseModel
 
 class CurrentDateTimeToolSpec(BaseToolSpec):
     """Current date and time tool spec.
 
     """
 
-    spec_functions = ["current_date", "current_time"]
+    spec_functions = ["current_date_and_time"]
 
-    def current_date(self, format:str="%A, %B %d, %Y", timezone: str="localtime") -> str:
+    def current_date_and_time(self, format:str="%A, %B %d, %Y %H:%M:%S", timezone: str="localtime") -> str:
         """
-        A usefull function that takes as input the date format as optional parameter, the timezone with default to the system locale, and returns the current date.
+        A usefull function that takes as input the date and time format as optional parameter, the timezone with default to the system locale, and returns the current date and time.
         """
         tz=ZoneInfo(timezone)
         return datetime.datetime.now(tz).strftime(format)
 
-    def current_time(self, format:str="%H:%M:%S", timezone: str="localtime") -> str:
-        """
-        A usefull function that takes as input the time format as optional parameter, the timezone with default to the system locale, and returns the time in the GMT timezone.
-        """
-        tz=ZoneInfo(timezone)
-        return datetime.datetime.now(tz).strftime(format)
+class DatetimeToolFnSchema(BaseModel):
+    """Default tool function Schema."""
+
+    format: str
+    timezone: str
