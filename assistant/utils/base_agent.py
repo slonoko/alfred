e16e52a5dbd3b9a2b84a81cd45f627d192675510
Elie Khoury <elie.kh@gmail.com>
2025-01-26 15:52:11 +0000

    using ollama

--- assistant/utils/base_agent.py
+++ assistant/utils/base_agent.py
@@ -28,7 +28,7 @@ class BaseAgent:
         self.ollama_llm, self.ollama_embedding, _ = initialize_ollama_services()
 
         Settings.embed_model = self.ollama_embedding
-        Settings.llm = self.azure_llm
+        Settings.llm = self.ollama_llm
 
         self.prompt = read_md_file(os.path.join(os.getcwd(), prompt_file))
 
